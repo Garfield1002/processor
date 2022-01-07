41c9cfcea3d739c4f48387245ebdac2820047d3a
Royer Jack <jack.royer.23@gmail.com>
2022-01-07 10:21:12 +0100

    added a simulatate script

---
+++ implem/simulate.sh
@@ -0,0 +1,4 @@
+git pull
+export PATH=${PATH}:/opt/riscv-cep-tools/bin
+source /tools/Xilinx/Vivado/2019.1/settings64.sh
+make simulation PROG=lui
--- simulate.sh
+++
@@ -1,2 +0,0 @@
-export PATH=${PATH}:/opt/riscv-cep-tools/bin
-source /tools/Xilinx/Vivado/2019.1/settings64.sh
